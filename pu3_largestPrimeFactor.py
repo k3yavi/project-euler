c897651e628812804a29c64947f1467c3ea05d27
avisrivastava <avi.srivastava@stonybrook.edu>
2017-08-12 10:43:50 -0400

    moving things

--- pu3_largestPrimeFactor.py
+++
@@ -1,7 +0,0 @@
-#!/bin/python2.7
-"This is the solution for third problem of project euler"
-
-"Problem 3: The Prime factors of 13195 are 5, 7, 13 and 29".
-"What is the largest prime factor of the number 600851475143"
-
-
